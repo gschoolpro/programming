a82575350f9cb1338aef2945f754ce03e24b06bc
gschoolpro <49395918+gschoolpro@users.noreply.github.com>
2019-07-24 20:48:16 +0900

    Add files via upload

---
+++ class_one/lcd_servo_buzzer/buzzer.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set #18 as buzzer pin
+BuzzerPin = 18
+
+def setup():
+    # Set the GPIO modes to BCM Numbering
+    GPIO.setmode(GPIO.BCM)
+    # Set BuzzerPin's mode to output,
+    # and initial level to High(3.3v)
+    GPIO.setup(BuzzerPin, GPIO.OUT, initial=GPIO.HIGH)
+
+# main関数部分
+def main():
+    while True:
+    	GPIO.output(BuzzerPin, GPIO.LOW)
+        time.sleep(0.3)
+        GPIO.output(BuzzerPin, GPIO.HIGH)
+        time.sleep(0.3)
+
+# destroy関数部分
+def destroy():
+    # Turn off buzzer
+    GPIO.output(BuzzerPin, GPIO.HIGH)
+    # Release resource
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        main()
+    # When 'Ctrl+C' is pressed, the child program
+    # destroy() will be  executed.
+    except KeyboardInterrupt:
+        destroy()
---
+++ class_one/lcd_servo_buzzer/servomotor_re.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 17
+
+def sensor_init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(servoPIN, GPIO.OUT)
+
+    p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+    p.start(2.5) # Initialization
+
+def main():
+    while True:
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(12.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(10)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(2.5)
+        time.sleep(0.5)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        p.stop()
+    finally:
+        GPIO.cleanup()
