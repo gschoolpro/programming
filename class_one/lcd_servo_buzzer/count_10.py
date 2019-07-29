012397ff598a2efeb4927c950861d331fd27791c
gschoolpro <49395918+gschoolpro@users.noreply.github.com>
2019-07-29 13:06:45 +0900

    ex of kadai4-4

---
+++ class_one/lcd_servo_buzzer/count_10.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+
+# Set #18 as buzzer pin
+BuzzerPin = 18
+
+def setup():
+    # Set the GPIO modes to BCM Numbering
+    GPIO.setmode(GPIO.BCM)
+    # Set BuzzerPin's mode to output, 
+    # and initial level to High(3.3v)
+    GPIO.setup(BuzzerPin, GPIO.OUT, initial=GPIO.HIGH)
+
+def count():
+    #for countdown
+    count = 10
+    while count != 0:
+	print count
+    	time.sleep(1)
+	count = count - 1
+
+
+def main():
+    print "Time UP!"
+    print 'Ctrl+C : end the program...'
+    while True:
+    	GPIO.output(BuzzerPin, GPIO.LOW)
+        time.sleep(0.05)
+        GPIO.output(BuzzerPin, GPIO.HIGH)
+        time.sleep(0.05)
+      
+
+def destroy():
+    # Turn off buzzer
+    GPIO.output(BuzzerPin, GPIO.HIGH)
+    # Release resource
+    GPIO.cleanup()    
+
+# If run this script directly, do:
+if __name__ == '__main__':
+    setup()
+    try:
+	count()
+        main()
+    # When 'Ctrl+C' is pressed, the child program 
+    # destroy() will be  executed.
+    except KeyboardInterrupt:
+        destroy()
+
