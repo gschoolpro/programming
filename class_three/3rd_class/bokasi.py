b1fa1c98f9eb05117e827e7c3105a8c9659ac8ed
gschoolpro <it-program@g-sch.jp>
2020-02-15 20:26:44 +0900

    image

--- class_three/3rd_class/bokasi.py
+++ class_three/3rd_class/bokasi.py
@@ -1,19 +1,18 @@
 import cv2
 import numpy as np
 def bokasi(image,height,width):
-    for w in range(width-1):
-        for h in range(height-1):
-            if w > 3 and w < width-3:
-                if h > 3 and h < height-3:    
-                    image[h,w,0] = np.sum(image[h-2:h+2,w-2:w+2,0])/16
-                    image[h,w,1] = np.sum(image[h-2:h+2,w-2:w+2,1])/16
-                    image[h,w,2] = np.sum(image[h-2:h+2,w-2:w+2,2])/16
-    image = cv2.resize(image,(int(width),int(height)))
+    for n in range(3):
+        for w in range(3,width-4):
+            for h in range(3,height-4):
+                image[h,w,0] = np.sum(image[h-2:h+2,w-2:w+2,0])/16
+                image[h,w,1] = np.sum(image[h-2:h+2,w-2:w+2,1])/16
+                image[h,w,2] = np.sum(image[h-2:h+2,w-2:w+2,2])/16
     return image
 
-image = cv2.imread('../gschool.jpeg',cv2.IMREAD_COLOR)
+image = cv2.imread('./hannin.png',-1)
 height = image.shape[0] 
 width = image.shape[1]
+image[np.where((image[0:height,0:width,3] == 0))] = [255,255,255,255] 
 image = bokasi(image,height,width)
 cv2.imshow('bokasi',image)
 cv2.waitKey()
--- class_three/3rd_class/mesen.py
+++ class_three/3rd_class/mesen.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 def mesen(image,height,width):
-    cv2.rectangle(image,(100,30),(160,50),(0,0,0),thickness=-1)
-    image = cv2.resize(image,(int(width),int(height)))
+    cv2.rectangle(image,(100,330),(220,365),(0,0,0),thickness=-1)
     return image
 
-image = cv2.imread('../gschool.jpeg',cv2.IMREAD_COLOR)
+image = cv2.imread('./hannin.png',-1)
 height = image.shape[0] 
 width = image.shape[1]
+image[np.where((image[0:height,0:width,3] == 0))] = [255,255,255,255] 
 image = mesen(image,height,width)
 cv2.imshow('mesen',image)
 cv2.waitKey()
